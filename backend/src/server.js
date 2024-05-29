ef425b28348c3488c6d6d50d8cb1ae0f50b8afac
J.M. Cléry <jmclery@gmail.com>
2024-05-29 14:38:23 +0200

    Display bikes in Home

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
 import 'dotenv/config'
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
 
 import { startApp, log, exitWithError } from './helpers.js';
 import router from './router.js'
 
 const app = express();
 
+app.use('/api', cors());
 app.use('/api', router);
 
 mongoose
--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,12 +1,49 @@
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+  const [stations, setStations] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:9000/api/stations')
+      .then((res) => res.json())
+      .then((stationsData) => {
+        setStations(stationsData);
+      });
+  }, []);
+
   return (
     <div className="container">
-      <h1>Hello React</h1>
+      <h1>Liste des stations Vélib Parisiennes</h1>
       <hr />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque modi delectus reprehenderit enim odio fugiat.
-        Vitae, cupiditate quas. Facilis iusto accusantium corporis et error porro eligendi, earum impedit quia libero.
-      </p>
+
+      <table className="stations-list">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Capacité</th>
+            <th>Nom</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stations &&
+            stations.map((station) => (
+              <tr key={station._id}>
+                <td>{station.stationcode}</td>
+                <td>{station.capacity} places</td>
+                <td>
+                  📌{' '}
+                  <a
+                    target="_blank"
+                    href={`https://www.google.com/maps/search/${station.coordonnees_geo.lat}+${station.coordonnees_geo.lon}`}
+                    title="Voir l'emplacement sur Google Maps"
+                  >
+                    {station.name}
+                  </a>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }
